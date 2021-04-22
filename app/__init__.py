0f23f08ee28071dc0394e2277141d36186d7d1ca
Vrushabh Raorane <59229108+Vrushabh6300@users.noreply.github.com>
2021-04-22 18:06:23 +0530

    Flask Web App code files

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '361788b7f6af4f12e0a7bf6074cca6f6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from app import routes
